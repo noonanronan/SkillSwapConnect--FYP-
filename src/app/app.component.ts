31d5a0deaaa3e5d7543c7ea5cad7595c1dd2e636
Ronan <ronannoonan322@yahoo.com>
2023-11-11 20:03:49 +0000

    Added to home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-// Import necessary modules from Angular
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  // Your component properties and methods here
+
+  ngOnInit() {
+    // Component initialization
+  }
+
+  ngOnDestroy() {
+    // Cleanup logic
+  }
 }
