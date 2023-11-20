07ffa86bd7043f89c63ba9a0642adf6504d49add
Ronan <ronannoonan322@yahoo.com>
2023-11-20 20:55:42 +0000

    Moved menu button left header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,6 +27,7 @@ const routes: Routes = [
     path: 'reset-password',
     loadChildren: () => import('./pages/reset-password/reset-password.module').then( m => m.ResetPasswordPageModule)
   },
+  
 ];
 
 @NgModule({
