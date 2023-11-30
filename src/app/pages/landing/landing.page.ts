cc93b8f2fb10ed967b0bb7742a04bc1f9ec254f4
Ronan <ronannoonan322@yahoo.com>
2023-11-30 14:04:00 +0000

    Feature cards now display message when clicked

--- src/app/pages/landing/landing.page.ts
+++ src/app/pages/landing/landing.page.ts
@@ -1,16 +1,33 @@
-// Import necessary Angular modules.
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-landing',                // This specifies the custom tag name for this component.
-  templateUrl: './landing.page.html',     // Path to the component's HTML template.
-  styleUrls: ['./landing.page.scss'],     // Path to the component's styles.
+  selector: 'app-landing',
+  templateUrl: './landing.page.html',
+  styleUrls: ['./landing.page.scss'],
 })
-export class LandingPage implements OnInit {
+export class LandingPage {
+  // Hold messages for each feature card - They are initialized to empty strings.
+  learnMessage: string = '';
+  shareMessage: string = '';
+  connectMessage: string = '';
 
   constructor() { }
 
-  ngOnInit() {
+  // Method to show the learn message - Clears after 2 seconds.
+  showLearnMessage(): void {
+    this.learnMessage = 'Learn and expand your knowledge!';
+    setTimeout(() => this.learnMessage = '', 2000);
   }
 
+  // Method to show the share message - Clears after 2 seconds.
+  showShareMessage(): void {
+    this.shareMessage = 'Share your skills and learn from others!';
+    setTimeout(() => this.shareMessage = '', 2000);
+  }
+
+  // Method to show the connect message - Clears after 2 seconds.
+  showConnectMessage(): void {
+    this.connectMessage = 'Connect with peers and build your network!';
+    setTimeout(() => this.connectMessage = '', 2000);
+  }
 }
