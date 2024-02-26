0b805c9cba828ead6dbfcdb1793435955c01cd23
Ronan <ronannoonan322@yahoo.com>
2024-02-26 15:45:07 +0000

    Refactor landing page layout and update CTA button positioning

--- src/app/pages/landing/landing.page.ts
+++ src/app/pages/landing/landing.page.ts
@@ -6,28 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing.page.scss'],
 })
 export class LandingPage {
-  // Hold messages for each feature card - They are initialized to empty strings.
-  skillMessage: string = '';
-  swapMessage: string = '';
-  connectMessage: string = '';
 
   constructor() { }
 
-  // Method to show the learn message - Clears after 2 seconds.
-  showSkillMessage(): void {
-    this.skillMessage = 'Share your skills and learn from others!';
-    setTimeout(() => this.skillMessage = '', 2000);
-  }
-
-  // Method to show the share message - Clears after 2 seconds.
-  showSwapMessage(): void {
-    this.swapMessage = 'Meeting friends!';
-    setTimeout(() => this.swapMessage = '', 2000);
-  }
-
-  // Method to show the connect message - Clears after 2 seconds.
-  showConnectMessage(): void {
-    this.connectMessage = 'Connect with peers and build your network!';
-    setTimeout(() => this.connectMessage = '', 2000);
-  }
+ 
 }
