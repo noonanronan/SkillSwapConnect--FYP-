e2538aac77b114cb1e42eb952d95259457db311b
John French <john.french@atu.ie>
2024-03-06 13:57:51 +0000

    Simple WebSocketConfigurer to test

--- backend/messaging-service/src/main/java/io/github/RazzaNoonan/messagingservice/WebSocketConfig.java
+++ backend/messaging-service/src/main/java/io/github/RazzaNoonan/messagingservice/WebSocketConfig.java
@@ -1,23 +1,41 @@
 package io.github.RazzaNoonan.messagingservice;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+//@Configuration
+//@EnableWebSocketMessageBroker
+//public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+//
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//    	registry.addEndpoint("/ws").setAllowedOrigins("*");
+//    }
+//
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//        registry.enableSimpleBroker("/topic");
+//        registry.setApplicationDestinationPrefixes("/app");
+//    }
+//}
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-    	registry.addEndpoint("/ws").setAllowedOrigins("*");
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myHandler(), "/ws2");
+
     }
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
+    @Bean
+    public WebSocketHandler myHandler() {
+        return new MyHandler();
     }
 }
+
