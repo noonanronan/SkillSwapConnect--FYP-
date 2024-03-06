2dfe76bf48260bd3f31bb2b9e026d070872872dd
Ronan <ronannoonan322@yahoo.com>
2024-03-06 20:35:53 +0000

    Access token being use to verify users

--- backend/messaging-service/src/main/java/io/github/RazzaNoonan/messagingservice/WebSocketConfig.java
+++ backend/messaging-service/src/main/java/io/github/RazzaNoonan/messagingservice/WebSocketConfig.java
@@ -2,46 +2,17 @@ package io.github.RazzaNoonan.messagingservice;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-<<<<<<< HEAD
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.config.annotation.*;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-=======
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
->>>>>>> main
 
-//@Configuration
-//@EnableWebSocketMessageBroker
-//public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-//
-//    @Override
-//    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//    	registry.addEndpoint("/ws").setAllowedOrigins("*");
-//    }
-//
-//    @Override
-//    public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        registry.enableSimpleBroker("/topic");
-//        registry.setApplicationDestinationPrefixes("/app");
-//    }
-//}
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-<<<<<<< HEAD
-        registry.addHandler(myHandler(), "/ws2");
-
-=======
         registry.addHandler(myHandler(), "/ws").setAllowedOrigins("*");
->>>>>>> main
     }
 
     @Bean
@@ -49,7 +20,3 @@ public class WebSocketConfig implements WebSocketConfigurer {
         return new MyHandler();
     }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> main
