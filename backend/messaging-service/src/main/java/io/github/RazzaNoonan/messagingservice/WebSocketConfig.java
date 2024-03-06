ac8bdde3ed05a9451f8e40ecd9c97e1d89b410be
Ronan <ronannoonan322@yahoo.com>
2024-03-06 19:14:16 +0000

    Message being recieved on server

--- backend/messaging-service/src/main/java/io/github/RazzaNoonan/messagingservice/WebSocketConfig.java
+++ backend/messaging-service/src/main/java/io/github/RazzaNoonan/messagingservice/WebSocketConfig.java
@@ -1,23 +1,22 @@
 package io.github.RazzaNoonan.messagingservice;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-    	registry.addEndpoint("/ws").setAllowedOrigins("*");
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myHandler(), "/ws").setAllowedOrigins("*");
     }
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
+    @Bean
+    public WebSocketHandler myHandler() {
+        return new MyHandler();
     }
 }
